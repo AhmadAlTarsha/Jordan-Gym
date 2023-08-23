f874a9d5ec7decb8e58b8c5d21f11044d6beb391
AhmadALTarsha <ahmadwael.altarsha@gmail.com>
2023-08-24 00:07:00 +0300

    start frontEnd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,27 @@
 import "./App.css";
-
+import React, { useState,useEffect ,createContext  } from "react";
+import axios from 'axios';
+import { Routes, Route, Link, useParams, useNavigate } from "react-router-dom";
+export const AppContext = createContext();
 function App() {
+  const [token,setToken]=useState(localStorage.getItem("token"))
+
+  axios.get("http://localhost:5000/gym/posts").then((res)=>{
+    console.log(res.data.gym
+       );
+       const a=res.data.gym
+       console.log(a[0].mempershipPrice);
+     
+  })
   return (
-    <div className="App">
+    <><AppContext.Provider value={""}>
+
+<div className="App">
       <h1>Hello, World!</h1>
     </div>
+    </AppContext.Provider>
+    </>
+  
   );
 }
 
